be2c81c005cf3862c12907ecb1b095be2ae9ad10
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2021-07-25 05:24:29 -0600

    Added the main body

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,7 +1,50 @@
 package numbers;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//        write your code here
+    // write your code here
+        int number;
+        String evenOrOdd;
+        boolean endsWith7;
+        boolean divisibleBySeven;
+        String explanation;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a natural number:");
+
+        number = scanner.nextInt();
+
+        if (number < 1) {
+            System.out.println("This number is not natural!");
+        }
+
+        else {
+            // Check if even or not
+            evenOrOdd = (number % 2 == 0) ? "Even" : "Odd";
+
+            System.out.println("This number is ".concat(evenOrOdd).concat("."));
+
+            divisibleBySeven = number % 7 == 0;
+
+            endsWith7 = (number < 10) ? (number == 7) : ((number - 7) % 10 == 0);
+
+            if (divisibleBySeven && endsWith7) {
+                explanation = number + " is divisible by 7 and ends with 7.";
+
+            } else if (!divisibleBySeven && endsWith7) {
+                explanation = number + " ends with 7.";
+
+            } else if (divisibleBySeven) {
+                explanation = number + " is divisible by 7.";
+
+            } else {
+                explanation = number + " is neither divisible by 7 nor does it end with 7.";
+            }
+
+            System.out.println(explanation);
+        }
+
     }
 }
