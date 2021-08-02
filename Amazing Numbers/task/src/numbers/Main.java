f9b483f0ab5d6ad345ccc109732b6137ee6051d1
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2021-08-02 14:45:01 -0600

    Added palindromic

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -5,55 +5,124 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     // write your code here
-        int number;
-        String evenOrOdd;
+        long number;
+        boolean isOdd;
+        boolean isEven;
         boolean endsWith7;
         boolean divisibleBySeven;
-        String explanation;
-        String result;
+        boolean isBuzz;
+        boolean isDuck;
+        boolean isPalindromic;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a natural number:");
 
-        number = scanner.nextInt();
+        System.out.println("Welcome to Amazing Numbers!");
+        System.out.println();
+        System.out.println("Supported requests:");
+        System.out.println("- enter a natural number to know its properties;");
+        System.out.println("- enter 0 to exit.");
 
-        // Checks if the number is a natural number
-        if (number < 1) {
-            System.out.println("This number is not natural!");
-        }
+        while(true) {
 
-        else {
-            // Check if even or not
-            evenOrOdd = (number % 2 == 0) ? "Even" : "Odd";
+            System.out.println();
+            System.out.print("Enter a request : ");
+            number = scanner.nextLong();
 
-            System.out.println("This number is ".concat(evenOrOdd).concat("."));
+            /* Checks if the number
+            is a natural number */
+            if (number > 0) {
 
-            // Checks if the number divisible by 7
-            divisibleBySeven = number % 7 == 0;
+                /* Checks if the number
+                is an even number */
+                isEven = number % 2 == 0;
+                isOdd = !isEven;
 
-            // Checks if the number ends with 7
-            endsWith7 = (number < 10) ? (number == 7) : ((number - 7) % 10 == 0);
+                // Checks if the number divisible by 7
+                divisibleBySeven = number % 7 == 0;
 
-            if (divisibleBySeven && endsWith7) {   // Checks if the number divisible by 7 and ends with 7
-                result = "It is a Buzz number.";
-                explanation = number + " is divisible by 7 and ends with 7.";
+                // Checks if the number ends with 7
+                endsWith7 = (number < 10) ? (number == 7) : ((number - 7) % 10 == 0);
+
+                // Checks if the number divisible by 7 and ends with 7
+                if (divisibleBySeven && endsWith7) {
+                    isBuzz = true;
+
+                } else // Checks if the number ends with 7
+                    if (!divisibleBySeven && endsWith7) {
+                        isBuzz = true;
 
 
-            } else if (!divisibleBySeven && endsWith7) { // Checks if the number ends with 7
-                result = "It is a Buzz number.";
-                explanation = number + " ends with 7.";
+                    } //Checks if the number divisible by 7
+                    else isBuzz = divisibleBySeven;
 
-            } else if (divisibleBySeven) {   // Checks if the number divisible by 7
-                result = "It is a Buzz number.";
-                explanation = number + " is divisible by 7.";
 
-            } else {
-                result = "It is not a Buzz number.";
-                explanation = number + " is neither divisible by 7 nor does it end with 7.";
+                /* Checks if the number
+                is a duck number */
+                String numberString = Long.toString(number);
+                int numberStringLength = numberString.length();
+                isDuck = false;
+
+                for (int i = 0; i < numberStringLength; i ++) {
+                    char currentDigit = numberString.charAt(i);
+
+                    if (currentDigit == '0') {
+                        isDuck = true;
+                        break;
+                    }
+
+                }
+
+                /* Checks if the number
+                is a Palindromic number */
+                StringBuilder reversedNumber = new StringBuilder();
+
+                for (int i = numberStringLength; i > 0 ; i --) {
+                    reversedNumber.append(numberString.charAt(i - 1));
+                }
+                isPalindromic = reversedNumber.toString().equals(numberString);
+
+
+                // Adds commas to the number as appropriate.
+                StringBuilder numberWithComma = new StringBuilder();
+
+
+                for (int i = numberStringLength; i > 0; i --) {
+
+                    if ((numberStringLength == i)) {
+                        numberWithComma.insert(0,numberString.charAt(i-1));
+                    }
+
+                    else if (((numberStringLength - i) % 3) == 0) {
+                        numberWithComma.insert(0,',');
+                        numberWithComma.insert(0,numberString.charAt(i-1));
+
+                    }
+
+                    else{
+                        numberWithComma.insert(0,numberString.charAt(i-1));
+                    }
+
+                }
+
+                System.out.println("Properties of " + numberWithComma);
+                System.out.println("\t    even: " + isEven );
+                System.out.println("\t    odd : " + isOdd );
+                System.out.println("\t    buzz: " + isBuzz );
+                System.out.println("\t    duck: " + isDuck );
+                System.out.println(" palindromic: " + isPalindromic );
+
             }
 
-            System.out.println(result.concat("\nExplanation:\n").concat(explanation));
+            else if (number == 0) {
+                System.out.println("Goodbye!");
+                break;
+            }
+
+            else {
+                System.out.println("The first parameter should be a natural number or zero.");
+            }
         }
+
 
     }
 }
